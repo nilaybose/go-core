e1bc08e7758f591fc9dc032b6bde4a42c65e380c
Nilay Bose <nilay.bose@gmail.com>
2022-05-15 02:29:26 +0530

    channel

---
+++ channel/channel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		ch <- 42
+	}()
+
+	fmt.Println(<-ch)
+}
---
+++ utils/nb-utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func Info() {
+	fmt.Println("Utilities to be imported")
+}
